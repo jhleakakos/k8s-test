ccc9802b4c72d2bf10ac39b26a863791cbe76cd7
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-09-04 15:08:45 -0600

    dockerize go app

--- go/app.go
+++ go/app.go
@@ -6,7 +6,7 @@ import (
 )
 
 func home(w http.ResponseWriter, _ *http.Request) {
-	io.WriteString(w, `<p style="text-align: center; padding-top: 10%;">Aren\'t you a good k8s: <b style="font-size: 2rem;">v1 from golang</b></p>`)
+	io.WriteString(w, `<p style="text-align: center; padding-top: 10%;">Aren't you a good k8s: <b style="font-size: 2rem;">v1 from golang</b></p>`)
 }
 
 func main() {
