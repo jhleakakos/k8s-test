8d0a851c3aa81575119e9cd35a93644749196f15
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-09-04 10:59:04 -0600

    refactor into node directory to set up for other apps in same repo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Small test for Kubernetes.
+
+# Command Reminders
+
+- `docker build -t <image_name> .` from within directory with Dockerfile to build the image
+- `docker compose up -d` and `docker compose down` to use compose to run individual containers
+- `kubectl apply -f <deployment_file_yaml>` to deploy with k8s
--- node/index.js
+++ node/index.js
@@ -4,5 +4,5 @@ const app = express();
 app.listen(3000, () => console.log('express listening on port 3000'));
 
 app.get('/', (req, res) => {
-    res.send('<p style="text-align: center; padding-top: 10%;">Aren\'t you a good k8s: <b style="font-size: 2rem;">v1</b></p>');
+    res.send('<p style="text-align: center; padding-top: 10%;">Aren\'t you a good k8s: <b style="font-size: 2rem;">v1 from node</b></p>');
 });
